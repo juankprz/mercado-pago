eb4dea1ea0e486551898df50fac21aa213e5a5e6
Juan Camilo Perez <juancamiloperez2009@hotmail.com>
2021-05-26 11:49:22 -0500

    Update TraitMercadoPago.php

--- app/Http/Traits/TraitMercadoPago.php
+++ app/Http/Traits/TraitMercadoPago.php
@@ -19,7 +19,7 @@ trait TraitMercadoPago
         /**
          * Summary: [AccessToken setter to MercadoPago\SDK Model]
          */
-
+        /* Ojo Señor, con las keys de servicios.  */
         MercadoPago\SDK::setAccessToken('APP_USR-2572771298846850-120119-a50dbddca35ac9b7e15118d47b111b5a-681067803');
         MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
         $preference = new MercadoPago\Preference();
